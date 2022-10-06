d4cd8880476a45156337d579d3df331faea0e269
MelvDouc <melv.douc@gmail.com>
2022-10-07 00:46:41 +0200

    testing branch2

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,4 @@ import PresenceUpdater from "./components/PresenceUpdater.js";
 customElements.define("presence-updater", PresenceUpdater, { extends: "input" });
 customElements.define("delete-player-form", DeletePlayerForm, { extends: "form" });
 
-console.log("branch 2!");
+console.log("N3");
