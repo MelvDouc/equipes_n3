adce138a8c21c001fb6442b8a7d694390c274a6b
MelvDouc <melv.douc@gmail.com>
2022-10-07 00:42:15 +0200

    remove dotenv from prod

--- dist/database/db.js
+++ dist/database/db.js
@@ -1,15 +1,21 @@
 import { MongoClient } from "mongodb";
 import MongoStore from "connect-mongo";
-import { config } from "dotenv";
-config();
-const uri = process.env.DB_URI;
-const client = new MongoClient(uri);
-await client.connect();
-console.log("Database connected.");
-const store = MongoStore.create({
-    mongoUrl: uri
-});
-const db = client.db(process.env.DB_NAME);
+async function Db() {
+    if (process.env.NODE_ENV !== "production")
+        (await import("dotenv")).config();
+    const uri = process.env.DB_URI;
+    const client = new MongoClient(uri);
+    await client.connect();
+    console.log("Database connected.");
+    const store = MongoStore.create({
+        mongoUrl: uri
+    });
+    const db = client.db(process.env.DB_NAME);
+    return {
+        db, store
+    };
+}
+const { db, store } = await Db();
 export default {
     collection: db.collection.bind(db),
     store
