2db684201c5bc54095d22b34e7c6577a3207d423
MelvDouc <melv.douc@gmail.com>
2022-10-09 12:33:59 +0200

    Use ts on frontend

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-import DeletePlayerForm from "./components/DeletePlayerForm.js";
-import PresenceUpdater from "./components/PresenceUpdater.js";
-import PlayerFetcher from "./components/PlayerFetcher.js";
-
-customElements.define("presence-updater", PresenceUpdater, { extends: "input" });
-customElements.define("delete-player-form", DeletePlayerForm, { extends: "form" });
-customElements.define("player-fetcher", PlayerFetcher);
