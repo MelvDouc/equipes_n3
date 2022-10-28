8db5d9a073ecd2b071cfabdfcfd375eff8086f5c
MelvDouc <melv.douc@gmail.com>
2022-10-28 11:47:22 +0200

    Folder structure overhaul

--- server/middleware/flash.middleware.js
+++
@@ -1,8 +0,0 @@
-const setFlash = (req, res, next) => {
-    ["success", "errors"].forEach(key => {
-        const value = req.flash(key);
-        key ? (res.locals[key] = value) : (delete res.locals[key]);
-    });
-    next();
-};
-export default setFlash;
